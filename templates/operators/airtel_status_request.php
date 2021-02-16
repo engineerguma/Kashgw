ea149cf2a0d0d5ceac95ee5eb346a9b8f9f2768c
Brian Gumisirza <guma250@gmail.com>
2021-02-16 14:50:15 +0300

    AM status reference change

--- templates/operators/airtel_status_request.php
+++ templates/operators/airtel_status_request.php
@@ -2,5 +2,5 @@
 $airtel_status_request = '<COMMAND>
 <TYPE>TXNEQREQ</TYPE>
 <INTERFACEID>PALMKASH</INTERFACEID>
-<EXTTRID>'.$trans_data['transaction_id'].'</EXTTRID>
+<EXTTRID>'.$trans_data['transaction_reference_number'].'</EXTTRID>
 </COMMAND>';
